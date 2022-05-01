7031c2b7c2e0d21e5a5d0c026dd44855d39b8970
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-01 23:46:22 +0300

    save

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const a = document.querySelector('.item');
-console.log(a);
+const totalCategories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${totalCategories.length}`);
+
+const textHeader = document.querySelectorAll('.item h2');
+const totalElement = document.querySelectorAll(`.item ul`);
+
+for (let i = 0; i < textHeader.length; i += 1) {
+    console.log(`Category: ${textHeader[i].textContent}`);
+    console.log(`Elements: ${totalElement[i].children.length}`);
+};
