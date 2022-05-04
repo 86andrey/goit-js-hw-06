95ff3ad255813d03b43d4d2b6b3243f28e9a46a0
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-04 22:44:18 +0300

    save

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+    body: document.body,
+    btnChangeColor: document.querySelector('.change-color'),
+    colorName: document.querySelector('.color')
+}
+
+refs.btnChangeColor.addEventListener('click', changeColor);
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorName.textContent = getRandomHexColor();
+};
+
+
+
