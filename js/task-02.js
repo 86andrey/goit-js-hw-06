613d4317b6419054c52c5412002aee130b175071
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-08 12:05:26 +0300

    save

--- js/task-02.js
+++ js/task-02.js
@@ -9,13 +9,11 @@ const ingredients = [
 
 const list = document.querySelector("#ingredients");
 
-const createElementLi = (ingredients) => {
-  return ingredients.map((ingredient) => {
-    const titleEl = document.createElement('li');
-    titleEl.textContent = ingredient;
-    titleEl.classList.add('item');
-    return titleEl;
-  });
-};
-
+const createElementLi = ingredients.map((ingredient) => {
+  const titleEl = document.createElement('li');
+  titleEl.textContent = ingredient;
+  titleEl.classList.add('item');
+  return titleEl;
+});
+  
 list.append(...createElementLi);
